76a2cdab7380d50c145e0b21308b8f1b4c864069
Edwin Rodriguez <edwinrodriguez2603@icloud.com>
2026-01-18 16:40:22 -0600

    refactor: Add new routes

--- app/views/layouts/sidebar.php
+++ app/views/layouts/sidebar.php
@@ -5,9 +5,14 @@
     </div>
 
     <ul class="menu">
-        <li><a href=""><i class="bi bi-speedometer2"></i> Dashboard</a></li>
-        <li><a href=""><i class="bi bi-cart"></i> Apartados</a></li>
-        <li><a href=""><i class="bi bi-box-seam"></i> Productos</a></li>
+        <li><a href="<?= BASE_URL ?>"><i class="bi bi-house"></i> Inicio</a></li>
+        <li><a href="<?= BASE_URL ?>perfil"><i class="bi bi-person-vcard"></i> Iniciar</a></li>
+        <li><a href="<?= BASE_URL ?>dashboard"><i class="bi bi-speedometer2"></i> Dashboard</a></li>
+        <li><a href="<?= BASE_URL ?>apartados"><i class="bi bi-cart"></i> Apartados</a></li>
+        <li><a href="<?= BASE_URL ?>productos"><i class="bi bi-box-seam"></i> Productos</a></li>
+        <li><a href="<?= BASE_URL ?>privacidad"><i class="bi bi-file-earmark-lock"></i>Privacidad</a></li>
+        <li><a href="<?= BASE_URL ?>subir_producto"><i class="bi bi-upload"></i>Subir producto</a></li>
+        <li><a href="<?= BASE_URL ?>inventario"><i class="bi bi-box-seam"></i>Inventario</a></li>
     </ul>
 </nav>
 
