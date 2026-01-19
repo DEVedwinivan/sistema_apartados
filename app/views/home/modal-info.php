0bfa6e028395a1efdf358431b1720c27a6c74e8d
Edwin Rodriguez <edwinrodriguez2603@icloud.com>
2026-01-18 18:18:44 -0600

    refactor: Modal information

--- app/views/home/modal-info.php
+++ app/views/home/modal-info.php
@@ -6,10 +6,12 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+        <p>Regalos Irma nació en 2010 con la visión de ofrecer regalos personalizados que transmitieran emociones y crearan recuerdos inolvidables. Desde nuestros inicios, hemos estado comprometidos con la calidad y la satisfacción del cliente, trabajando estrechamente con artesanos locales para diseñar productos únicos.</p>
+        <p>A lo largo de los años, hemos ampliado nuestra gama de productos y servicios, adaptándonos a las tendencias del mercado y a las necesidades de nuestros clientes. Nuestra misión es seguir siendo una empresa líder en el sector de regalos personalizados, ofreciendo experiencias excepcionales y productos que realmente marquen la diferencia.</p>
+        <p>Gracias por ser parte de nuestra historia y permitirnos ser parte de los momentos especiales en sus vidas.</p>        
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btns_home" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btns_home" data-bs-dismiss="modal">Cerrar</button>
       </div>
     </div>
   </div>
