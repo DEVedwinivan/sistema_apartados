3f1a76f19e98a4b96c381093ec151c84bc2f537f
Edwin Rodriguez <edwinrodriguez2603@icloud.com>
2026-01-18 18:34:29 -0600

    refactor: Route editing

--- app/views/layouts/sidebar.php
+++ app/views/layouts/sidebar.php
@@ -6,16 +6,16 @@
 
     <ul class="menu">
         <li><a href="<?= BASE_URL ?>"><i class="bi bi-house"></i> Inicio</a></li>
-        <li><a href="<?= BASE_URL ?>perfil"><i class="bi bi-person-vcard"></i> Iniciar Sesión</a></li>
-        <li><a href="<?= BASE_URL ?>mis_apartados"><i class="bi bi-cart"></i> Mis apartados</a></li>
-        <li><a href="<?= BASE_URL ?>productos"><i class="bi bi-box-seam"></i> Productos</a></li>
+        <li><a href="<?= BASE_URL ?>iniciar"><i class="bi bi-person-vcard"></i> Iniciar Sesión</a></li>
+        <li><a href="<?= BASE_URL ?>apartados/mis_apartados"><i class="bi bi-cart"></i> Mis apartados</a></li>
+        <li><a href="<?= BASE_URL ?>products/productos"><i class="bi bi-box-seam"></i> Productos</a></li>
         <li><a href="<?= BASE_URL ?>privacidad"><i class="bi bi-file-earmark-lock"></i>Privacidad</a></li>
         <!-- Apardado de administrador -->
-        <li><a href="<?= BASE_URL ?>usuarios"><i class="bi bi-people"></i>Usuarios</a></li>
-        <li><a href="<?= BASE_URL ?>dashboard"><i class="bi bi-speedometer2"></i> Dashboard</a></li>
-        <li><a href="<?= BASE_URL ?>apartados"><i class="bi bi-cart"></i> Apartados</a></li>
-        <li><a href="<?= BASE_URL ?>subir_producto"><i class="bi bi-upload"></i>Subir producto</a></li>
-        <li><a href="<?= BASE_URL ?>inventario"><i class="bi bi-box-seam"></i>Inventario</a></li>
+        <li><a href="<?= BASE_URL ?>admin/usuarios"><i class="bi bi-people"></i>Usuarios</a></li>
+        <li><a href="<?= BASE_URL ?>admin/dashboard"><i class="bi bi-speedometer2"></i> Dashboard</a></li>
+        <li><a href="<?= BASE_URL ?>admin/apartados"><i class="bi bi-cart"></i> Apartados</a></li>
+        <li><a href="<?= BASE_URL ?>admin/subir_producto"><i class="bi bi-upload"></i>Subir producto</a></li>
+        <li><a href="<?= BASE_URL ?>admin/inventario"><i class="bi bi-box-seam"></i>Inventario</a></li>
     </ul>
 </nav>
 
