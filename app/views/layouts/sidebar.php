76cdb3aa6ed69d13605b9b2e379f90061d5a6d9f
Edwin Rodriguez <edwinrodriguez2603@icloud.com>
2026-01-18 18:16:41 -0600

    feat: add what the administrator will have

--- app/views/layouts/sidebar.php
+++ app/views/layouts/sidebar.php
@@ -6,11 +6,14 @@
 
     <ul class="menu">
         <li><a href="<?= BASE_URL ?>"><i class="bi bi-house"></i> Inicio</a></li>
-        <li><a href="<?= BASE_URL ?>perfil"><i class="bi bi-person-vcard"></i> Iniciar</a></li>
-        <li><a href="<?= BASE_URL ?>dashboard"><i class="bi bi-speedometer2"></i> Dashboard</a></li>
-        <li><a href="<?= BASE_URL ?>apartados"><i class="bi bi-cart"></i> Apartados</a></li>
+        <li><a href="<?= BASE_URL ?>perfil"><i class="bi bi-person-vcard"></i> Iniciar Sesión</a></li>
+        <li><a href="<?= BASE_URL ?>mis_apartados"><i class="bi bi-cart"></i> Mis apartados</a></li>
         <li><a href="<?= BASE_URL ?>productos"><i class="bi bi-box-seam"></i> Productos</a></li>
         <li><a href="<?= BASE_URL ?>privacidad"><i class="bi bi-file-earmark-lock"></i>Privacidad</a></li>
+        <!-- Apardado de administrador -->
+        <li><a href="<?= BASE_URL ?>usuarios"><i class="bi bi-people"></i>Usuarios</a></li>
+        <li><a href="<?= BASE_URL ?>dashboard"><i class="bi bi-speedometer2"></i> Dashboard</a></li>
+        <li><a href="<?= BASE_URL ?>apartados"><i class="bi bi-cart"></i> Apartados</a></li>
         <li><a href="<?= BASE_URL ?>subir_producto"><i class="bi bi-upload"></i>Subir producto</a></li>
         <li><a href="<?= BASE_URL ?>inventario"><i class="bi bi-box-seam"></i>Inventario</a></li>
     </ul>
