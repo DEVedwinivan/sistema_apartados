64e3becde154f49086556ba76f91d174a121d7d8
Edwin Rodriguez <edwinrodriguez2603@icloud.com>
2026-01-26 21:58:38 -0600

    refactor: add the link for the user_form.js script

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,3 +1,4 @@
+<script src="<?= BASE_URL ?>assets/js/user_form.js"></script>
 <script src="<?= BASE_URL?>assets/js/sidebar.js"></script>
 <script src="<?= BASE_URL ?>assets/js/bootstrap.bundle.min.js"></script>
 </body>
