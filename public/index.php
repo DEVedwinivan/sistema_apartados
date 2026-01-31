cb1e6adbfaef5b8219493242780d29f36b95e24d
Edwin Rodriguez <edwinrodriguez2603@icloud.com>
2026-01-31 17:15:06 -0600

    refactor: Call the files Views.php and autoload.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
-require_once __DIR__ . '/../config/database.php';
-require_once __DIR__ . '/../core/Router.php';
 define('BASE_URL', 'http://localhost:8000/');
 define('ROOT', dirname(__DIR__));
+require_once __DIR__ . '/../core/Router.php';
+require_once __DIR__ . '/../app/autoload.php';
+require_once __DIR__ . '/../core/Views.php';
 $router = new Router();
 
 require_once __DIR__ . '/../routes/web.php';
