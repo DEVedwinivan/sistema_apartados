40b428362dac66bd48257677de2cc8e0b56f0565
Edwin Rodriguez <edwinrodriguez2603@icloud.com>
2026-02-01 22:21:36 -0600

    feat: Receive the data, call the model, decide which view to display

--- app/controllers/Authcontroller.php
+++ app/controllers/Authcontroller.php
@@ -1,13 +1,40 @@
 <?php
 class AuthController {
-    // private AuthModel $auth;
-    public function __construct() {
-    //     $db = db::dbConnect();
-    // $this->auth = new AuthModel(db::dbConnect());
+    private $model;
+    public function __construct()
+    {
+        $this->model = new AuthModel();
     }
-    public function showLogin(){
+    public function showForm(){
         Views::render('auth/login');
 
     }
+    public function login(){
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            header("Location:". BASE_URL . "iniciar");
+            exit;
+        }
+        $email = trim($_POST['emailL'] ?? '');
+        $password = $_POST['passwordL'] ?? '';
+
+        $user = $this->model->login($email, $password);
+            
+        if (!$user) {
+            $_SESSION['error'] = 'Correo o contraseña incorrectos';
+            header("Location:" . BASE_URL . "iniciar");
+            exit;
+        }
+
+        $_SESSION['user'] = [
+            'id' => $user['id'],
+            'nombre' => $user['nombre'],
+            'rol' => $user['rol']
+        ];
+
+        header('Location:' . BASE_URL);
+        exit;
+    }
+
+
 }
 ?>
