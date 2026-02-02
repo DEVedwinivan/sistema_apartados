d0c7a105cbe86125c26ad4de0612be93c4c760b6
Edwin Rodriguez <edwinrodriguez2603@icloud.com>
2026-02-02 11:14:23 -0600

    refactor add the route /registrar

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,6 @@
 $router->get('/', 'HomeController@index');
 
 $router->get('/iniciar', 'AuthController@showForm');
-$router->post('/iniciar', 'AuthController@login')
+$router->post('/iniciar', 'AuthController@login');
+$router->post('/registrar', 'AuthController@register');
 ?>
